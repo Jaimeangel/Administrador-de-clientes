93917861cda2edcca683914efd140e0747917ee9
Jaime A <jaimeangel1097@gmail.com>
2023-03-24 20:05:28 -0500

    creando formulario de nuevos clientes

--- src/Components/Cliente.jsx
+++ src/Components/Cliente.jsx
@@ -1,8 +1,24 @@
-function Cliente() {
+function Cliente({cliente}) {
+    const {nombre,empresa,email,telefono}= cliente;
   return (
-    <div>
-      
-    </div>
+    <tr className="text-black border-b border-blue-400">
+        <td className="py-3 pl-3">
+            <p><span className="font-bold uppercase">Name: </span>{nombre}</p>
+            <p><span className="font-bold uppercase">Empresa: </span>{empresa}</p>
+        </td>
+        <td className="py-3">
+            <p><span className="font-bold uppercase">Email: </span>{email}</p>
+            <p><span className="font-bold uppercase">Phone: </span>{telefono}</p>
+        </td>
+        <td className="py-3 space-x-3">
+            <button 
+              className="px-4 bg-green-500 rounded-md font-bold text-white tracking-wider"
+              >EDITAR</button>
+            <button 
+              className="px-4 bg-red-500 rounded-md font-bold text-white tracking-wider"
+              >DELETE</button>
+        </td>
+    </tr>
   )
 }
 
--- src/Pages/NuevoCliente.jsx
+++ src/Pages/NuevoCliente.jsx
@@ -1,7 +1,30 @@
+import {useNavigate,Form} from 'react-router-dom'
+import FormNuevoCliente from '../Components/FormNuevoCliente';
+
 function NuevoCliente(){
+  const navigate=useNavigate()
   return (
     <div>
-      <h1 className="font-bold text-6xl">Esto es un nuevo cliente</h1>
+      <h1 className="font-bold text-4xl">Crea un nuevo cliente</h1>
+      <p className="text-xl mt-3">Llena todos los campos para crear con exito un nuevo cliente</p>
+      <div className="flex justify-end">
+        <button
+          className="bg-blue-700 text-white font-bold px-5 py-1 rounded-md tracking-wider"
+          onClick={()=>navigate(-1)}
+        >
+          VOLVER
+        </button>
+      </div>
+      <div className="md:w-3/4 bg-white mx-auto mt-10 p-6 rounded-md shadow">
+        <Form className='flex flex-col items-center'>
+          <FormNuevoCliente/>
+          <input 
+            type="submit"
+            value="Registrar Cliente"
+            className="w-3/4 mt-3 px-6 py-2 text-lg bg-green-500 rounded-md font-bold text-white tracking-wider cursor-pointer uppercase" 
+          />
+        </Form>
+      </div>
     </div>
   )
 }
